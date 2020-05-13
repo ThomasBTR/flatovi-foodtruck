341d9236c6c4f88bf614bfd6a04fb516ab18e987
ThomasBTR <thomatito971@gmail.com>
2020-05-13 10:09:09 +0200

    Initial commit

---
+++ src/main/java/com/training/flatovifoodtruck/FlatoviFoodtruckApplication.java
@@ -0,0 +1,13 @@
+package com.training.flatovifoodtruck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatoviFoodtruckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlatoviFoodtruckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/training/flatovifoodtruck/FlatoviFoodtruckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.flatovifoodtruck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatoviFoodtruckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
